7c2511d62469e51bce120155847d22b2c52f711b
Hugo Tapia <hugoetq@hotmail.com>
2026-06-14 15:17:57 +0100

    Resuelto ej3a1 y actualizado .gitignore

--- 3a/ej3a1/ej3a1_main.py
+++ 3a/ej3a1/ej3a1_main.py
@@ -22,28 +22,6 @@ Ejemplo:
     1261
     4.5
 
-Enunciat:
-Implementa un mòdul anomenat 'ej3a1_operations.py' que contingui funcions
-matemàtiques bàsiques com 'add(num_1, num_2)', 'subtract(num_1, num_2)',
-'multiply(num_1, num_2)' i 'divide(num_1, num_2)'.
-El mòdul 'ej3a1_operations.py' serà cridat des de 'ej3a1_main.py'.
-
-Paràmetres:
-     - num_1: valor númeric al qual es realitzarà l'operació.
-     - num_2: valor númeric al qual es realitzarà l'operació.
-
-Exemple:
-     Entrada:
-     add(100, 575)
-     subtract(15, 461)
-     multiply(13, 97)
-     divide(54, 12)
-
-     Sortida:
-     675
-     -446
-     1261
-     4.5
 
 """
 
